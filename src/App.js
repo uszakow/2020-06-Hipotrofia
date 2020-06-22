c17222648c220f2380ee712c47211d09a0716779
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-06-22 16:11:26 +0200

    Menu modificated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Menu from './components/Menu/Menu';
+import Banner from './components/Banner/Banner';
+
 function App() {
   return (
     <>
+      <Menu />
+      <Banner />
     </>
   );
 }
