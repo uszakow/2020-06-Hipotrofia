3192a19f5a8d13fdb6a3e0793a07aa17061ebcc6
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-06-22 16:18:19 +0200

    App modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Home from './components/Home/Home';
+
 function App() {
   return (
     <>
+      <div className="app-container">
+        <Home />
+      </div>
     </>
   );
 }
