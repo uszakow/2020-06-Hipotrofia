393bb0fc4763af1ad2baf5396a0d6875deecdeee
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-06-22 11:09:43 +0200

    Build

---
+++ build/precache-manifest.f17e8627996fd331985e01c55420335f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90c30ef52b4857f64b1b4b6bff6d10e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e36cac75eb0cd00e6365",
+    "url": "/static/css/main.7fab302f.chunk.css"
+  },
+  {
+    "revision": "5a3815090d8a8cb0a04d",
+    "url": "/static/js/2.5444f720.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5444f720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36cac75eb0cd00e6365",
+    "url": "/static/js/main.c89d5c35.chunk.js"
+  },
+  {
+    "revision": "5d92cf722ac0a39626e5",
+    "url": "/static/js/runtime-main.7057cae1.js"
+  },
+  {
+    "revision": "73ddb10dd2086c10c17079d0014fb388",
+    "url": "/static/media/banner.73ddb10d.svg"
+  },
+  {
+    "revision": "e5ac24cb4b80df0b0b798ca003c3a26a",
+    "url": "/static/media/contact.e5ac24cb.svg"
+  },
+  {
+    "revision": "df9f61f1683c7b34e297d6c8eafdef10",
+    "url": "/static/media/img1.df9f61f1.svg"
+  },
+  {
+    "revision": "b46d9aa6db3732a87da849da3453e4cc",
+    "url": "/static/media/img2.b46d9aa6.svg"
+  },
+  {
+    "revision": "7879b63005f8fdabd1da7ca98d18ad57",
+    "url": "/static/media/img3.7879b630.svg"
+  },
+  {
+    "revision": "28a62c4b87ac4c12d5130a84a7bc7174",
+    "url": "/static/media/login.28a62c4b.svg"
+  },
+  {
+    "revision": "207eb1834e063954aa04701b94e587dc",
+    "url": "/static/media/picture.207eb183.svg"
+  },
+  {
+    "revision": "893bce00ec518b85ccd7ab4d92b739c8",
+    "url": "/static/media/search.893bce00.svg"
+  }
+]);
