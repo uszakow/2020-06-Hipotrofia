63431906fb23ab1b0c3ea52181b222ba2c649279
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-06-22 10:20:29 +0200

    Modification of main styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
 
+import Menu from './components/Menu/Menu';
+import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
+
+import Home from './components/Home/Home';
+
 function App() {
   return (
     <>
+      <Menu />
+      <Banner />
+      <div className="app-container">
+        <Home />
+      </div>
+      <Footer />
     </>
   );
 }
