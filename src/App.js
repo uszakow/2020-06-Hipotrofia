a2d261eea7bf69121365538f8c65f723f55d37fa
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-06-23 14:34:54 +0200

    Banner fixed, variables are in a separate file, project structure was changed - in App only pages and futer (menu and banner removed to pages).

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import Menu from './components/Menu/Menu';
-import Banner from './components/Banner/Banner';
-import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
     <>
-      <Menu />
-      <Banner />
-      <div className="app-container">
-        <Home />
-      </div>
+      <Home />
       <Footer />
     </>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Home.css';
 
+import Menu from '../Menu/Menu';
+import Banner from '../Banner/Banner';
 import Pictures from './HomePictures/Pictures';
 import Text from './HomeText/Text';
+
+import bannerPhoto from '../../img/Banner/banner.svg';
 
 function Home() {
     return (
         <>
+            <Menu />
+            <Banner photo={bannerPhoto}/>
             <Pictures />
             <Text />
         </>
