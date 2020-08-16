641e7a3332ce0c6007d2f547e09e8ea4aaa6d3b8
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-08-16 20:15:16 +0200

    Banner is normal and doen't destroy menu, if picture wouldn't download

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Banner(props) {
     return (
         <div className="banner-container">
-            <img src={props.photo} alt="background" width='100%' className="banner-background" />
+            <img src={props.photo} alt="" width='100%' className="banner-background" />
             <div className="banner-button-container">
                 <Link to="/pregnancy" className="banner-button banner-button-pregnancy">Hipotrofia w ciąży</Link>
                 <Link to="/children" className="banner-button banner-button-baby">Hipotrofia u dzieci</Link>
