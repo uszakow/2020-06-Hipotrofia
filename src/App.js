4b7beadaf2509e697aaa0b45f715af4ff3eaa191
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-08-16 19:42:57 +0200

    Each page has min-height 100%, so footer is on bottim in a short page (like 404). Links work in Home component.

--- src/App.js
+++ src/App.js
@@ -19,34 +19,39 @@ import Cooperation from './components/Cooperation';
 function App() {
   return (
     <Router>
-      <Menu />
+      <div className="app-wrap">
+        <Menu />
 
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/help">
-          <PageInConstruction />
-        </Route>
-        <Route path="/stories">
-          <PageInConstruction />
-        </Route>
-        <Route path="/blog">
-          <PageInConstruction />
-        </Route>
-        <Route path="/cooperation" component={Cooperation} />
-        <Route path="/login">
-          <PageInConstruction />
-        </Route>
-        <Route path="/pregnancy">
-          <PageInConstruction />
-        </Route>
-        <Route path="/children">
-          <PageInConstruction />
-        </Route>
-        <Route component={Page404} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/help">
+            <PageInConstruction />
+          </Route>
+          <Route path="/stories">
+            <PageInConstruction />
+          </Route>
+          <Route path="/blog">
+            <PageInConstruction />
+          </Route>
+          <Route path="/cooperation" component={Cooperation} />
+          <Route path="/login">
+            <PageInConstruction />
+          </Route>
+          <Route path="/pregnancy">
+            <PageInConstruction />
+          </Route>
+          <Route path="/children">
+            <PageInConstruction />
+          </Route>
+          <Route path="/inconstruction">
+            <PageInConstruction />
+          </Route>
+          <Route component={Page404} />
+        </Switch>
 
-      <Footer />
-      <ButtonUp />
+        <Footer />
+        <ButtonUp />
+      </div>
     </Router>
   );
 }
