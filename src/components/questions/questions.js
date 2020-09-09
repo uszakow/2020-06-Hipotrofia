38e5ee2b8b997f2272d33c02882862df383cf401
Maria <maria@wzyk.io>
2020-09-09 14:43:11 +0200

    in progress

--- src/components/questions/questions.js
+++ src/components/questions/questions.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Banner from "../Banner/Banner";
 import ButtonsAndText from '../ButtonsAndText/ButtonsAndText';
+import bannerPhoto from "../../img/Banner/banner.svg";
 
 const QuestionsPage = () => (
-    <div>
-    <Banner />
+    <>
+    <Banner photo={bannerPhoto}/>
+    <div className='questions container'>
     <ButtonsAndText />
 
     <div>
         hhhejjjjjjjj
     </div>
+    <div className="questions container_photo"></div>
     </div>
+    </>
 )
 
 export default QuestionsPage;
