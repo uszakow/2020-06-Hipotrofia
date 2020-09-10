b651541d2cdc4dc4e0a9f0212aabe7772d63f045
Maria <maria@wzyk.io>
2020-09-10 15:09:02 +0200

    buttons works

--- src/components/questions/QuestionsData/questionsData.js
+++ src/components/questions/QuestionsData/questionsData.js
@@ -3,7 +3,7 @@ import QuestionsButtons from '../questionsButtons/questionsButtons';
 
 
 const questionsAnswers = [
-  { question: "Question 1", answer: "1 lorem ipsum" },
+  { question: "Question 1", answer: "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
   { question: "Question 2", answer: "2 lorem ipsum" },
 ];
 
@@ -11,7 +11,7 @@ function QuestionsData() {
 return (
   <div className='questions-questionsData-container'>
     {questionsAnswers.map((item, index)=> (
-                <QuestionsButtons key={index} item={item} />
+                <QuestionsButtons key={index} item={item} no={index}/>
             ))}
   </div>
 )
--- src/components/questions/questionsButtons/questionsButtons.js
+++ src/components/questions/questionsButtons/questionsButtons.js
@@ -4,12 +4,12 @@ import "./questionsButtons.scss";
 function QuestionsButtons(props) {
     const { question, answer } = props.item;
     const showHide = () => {
-        alert('dziala');
+        document.getElementById(question).classList.toggle('hide');
     }
     return (
         <div>
         <button className='questionsButtons_button' onClick={showHide}>{question}</button>
-        <div>{answer}</div>
+        <div id={question} className='hide'>{answer}</div>
         </div>
     )
 }
