5599792054dfae6561b66e3ac95df2f7d23d284a
Maria <maria@wzyk.io>
2020-09-11 13:31:44 +0200

    in progress

--- src/components/questions/questionsButtons/questionsButtons.js
+++ src/components/questions/questionsButtons/questionsButtons.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./questionsButtons.scss";
-import Vector from '../../../img/questions/Vector.png'
+import Vector from "../../../img/questions/Vector.png";
 
 function QuestionsButtons(props) {
-    const { question, answer } = props.item;
-    const showHide = () => {
-        document.getElementById(question).classList.toggle('hide');
-        document.getElementById('img'+question).classList.toggle('rotate');
-
-    }
-    return (
-        <div>
-        <button className='questions_questionsButtons_button' onClick={showHide}>{question}<img src={Vector} className='rotate' id={'img' + question}/></button>
-        <div id={question} className='hide'>{answer}</div> {/*zmiana nazwy klasy na "questions_questionsButtons_hide" powoduje, że button nie działa, nie wiem dlaczego*/}
-        </div>
-    )
+  const { question, answer } = props.item;
+  const showHide = () => {
+    document.getElementById(question).classList.toggle("hide");
+    document.getElementById("img" + question).classList.toggle("rotate");
+  };
+  return (
+    <div>
+      <button className="questions_questionsButtons_button" onClick={showHide}>
+        <span className="questions_questionsButtons_question">{question}</span>
+        <span>
+          <img src={Vector} className="rotate, questions_questionsButtons_img" id={"img" + question} />
+        </span>
+      </button>
+      <div id={question} className="hide">
+        {answer}
+      </div>{" "}
+      {/*zmiana nazwy klasy na "questions_questionsButtons_hide" powoduje, że button nie działa, nie wiem dlaczego*/}
+    </div>
+  );
 }
 
 export default QuestionsButtons;
