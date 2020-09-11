ae6d8d85de1e929a009f4853500a04631d5fc48e
Maria <maria@wzyk.io>
2020-09-11 13:11:02 +0200

    arrow change works

--- src/components/questions/questionsButtons/questionsButtons.js
+++ src/components/questions/questionsButtons/questionsButtons.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import "./questionsButtons.scss";
+import Vector from '../../../img/questions/Vector.png'
 
 function QuestionsButtons(props) {
     const { question, answer } = props.item;
     const showHide = () => {
         document.getElementById(question).classList.toggle('hide');
+        document.getElementById('img'+question).classList.toggle('rotate');
+
     }
     return (
         <div>
-        <button className='questionsButtons_button' onClick={showHide}>{question}</button>
-        <div id={question} className='hide'>{answer}</div>
+        <button className='questions_questionsButtons_button' onClick={showHide}>{question}<img src={Vector} className='rotate' id={'img' + question}/></button>
+        <div id={question} className='hide'>{answer}</div> {/*zmiana nazwy klasy na "questions_questionsButtons_hide" powoduje, że button nie działa, nie wiem dlaczego*/}
         </div>
     )
 }
