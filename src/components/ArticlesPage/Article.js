e058ab849fb0da8b47572e1f9b0f032d6789ff09
karolinaskorupska <pixie22@op.pl>
2020-09-26 21:11:11 +0200

    styling

--- src/components/ArticlesPage/Article.js
+++ src/components/ArticlesPage/Article.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
 
 import HeartImage from "../../img/Articles/Vector.png";
+import ThumbUpImage from "../../img/Articles/thumbs-up.png";
+import MessageSquareImage from "../../img/Articles/message-square.png";
+import ShareImage from "../../img/Articles/share-2.png";
+import EditImage from "../../img/Articles/edit-3.png";
+import DeleteImage from "../../img/Articles/trash.png";
+import AvatarImage from "../../img/Articles/woman-wearing-coat-762020 1.png";
 
 class Article extends Component {
   render() {
     return (
-      <div className="ArticlesPage article">
+      <div className="article">
         <div className=" article-horizontal-line"></div>
         <div className=" article-content">
-          <div className=" article-content-avatar-image"></div>
+          <div className=" article-content-avatar-image">
+              <img src={AvatarImage}></img>
+          </div>
           <div className=" article-content-text">
             <div className=" article-content-text-header">
               <div className="article-content-text-header-box">
-                <h1>Tytuł artytułu</h1>
+                <h1>Tytuł artykułu</h1>
                 <p>Data utworzenia</p>
               </div>
 
@@ -20,29 +28,32 @@ class Article extends Component {
             </div>
             <div className="article-content-text-body-box">
                 <div className="article-content-text-body-box-content">treść artykułu blablabla</div>
-                <p>Czytaj więcej...</p>
+                <p>Czytaj więcej</p>
 
                 <p>Czytaj mniej</p>
+
+                <div className="horizontal-line-short"></div>
                 <div className="article-content-text-body-box-content-action-box">
-                    <div className="hand-up">
-                        <img></img>
-                        <p>Doceń</p>
+                    <div className="thumb-up">
+                        <img src={ThumbUpImage}></img>
+                        Doceń
+                        
                     </div>
                     <div className="comment">
-                        <img></img>
-                        <p>Skomentuj</p>
+                        <img src={MessageSquareImage}></img>
+                        Skomentuj
                     </div>
                     <div className="share">
-                        <img></img>
-                        <p>Udostępnij</p>
+                        <img src={ShareImage}></img>
+                        Udostępnij
                     </div>
                     <div className="edit">
-                        <img></img>
-                        <p>Edit</p>
+                        <img src={EditImage}></img>
+                        Edytuj
                     </div>
                     <div className="delete">
-                        <img></img>
-                        <p>Usuń</p>
+                        <img src={DeleteImage}></img>
+                        Usuń
                     </div>
                 </div>
             </div>
