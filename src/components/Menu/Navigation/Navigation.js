ad867a6a976a661cc73d6a735894ad3684e629a9
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-09-28 13:16:46 +0200

    Menu with fix according to figma project

--- src/components/Menu/Navigation/Navigation.js
+++ src/components/Menu/Navigation/Navigation.js
@@ -3,8 +3,6 @@ import './Navigation.scss';
 
 import MenuItem from '../MenuItem/MenuItem';
 
-import contact from '../../../img/Menu/contact.svg';
-
 function Navigation(props) {
     const { css, closeMenu } = props;
     const cssItem = (css === 'menu-navigation-mobile' ? 'menu-item-mobile' : 'menu-item-desctop');
@@ -14,12 +12,7 @@ function Navigation(props) {
             <MenuItem href="/" cssItem={cssItem} closeMenu={closeMenu}>Hipotrofia</MenuItem>
             <MenuItem href="/help" cssItem={cssItem} closeMenu={closeMenu}>Pomoc</MenuItem>
             <MenuItem href="/stories" cssItem={cssItem} closeMenu={closeMenu}>Historie dzieci</MenuItem>           
-            <MenuItem href="/cooperation" cssItem={cssItem} closeMenu={closeMenu}>
-                <span>
-                    <img src={contact} width="20" className="mobile-only" alt="contact" />
-                    Współpraca
-                </span>
-            </MenuItem>
+            <MenuItem href="/cooperation" cssItem={cssItem} closeMenu={closeMenu}>Współpraca</MenuItem>
         </nav>
     )
 }
